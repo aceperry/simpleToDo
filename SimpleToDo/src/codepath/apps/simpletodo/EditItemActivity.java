b9f52e5e245e90ce70e683cdc06e8a498dc483e6
perry <perry@raptor2>
2014-02-19 03:33:38 -0800

    All done, some trivial error correcting can be added.

--- SimpleToDo/src/codepath/apps/simpletodo/EditItemActivity.java
+++ SimpleToDo/src/codepath/apps/simpletodo/EditItemActivity.java
@@ -1,22 +1,39 @@
 package codepath.apps.simpletodo;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.app.Activity;
-import android.view.Menu;
+import android.content.Intent;
 
 public class EditItemActivity extends Activity {
-
+	private EditText editItem;
+	private String txt;
+	private int pos;
+	private final int WRONG_POS = -1;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_edit_item);
-	}
+		editItem = (EditText) findViewById(R.id.editItem);
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.edit_item, menu);
-		return true;
+		txt = getIntent().getStringExtra("itemText");
+		pos = getIntent().getIntExtra("pos", WRONG_POS);
+		editItem.setText(txt);
+			// Wire up button with event handler
+		final Button button = (Button) findViewById(R.id.saveEditButton);
+		button.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+                String newText = (String) editItem.getText().toString();
+                Intent nString = new Intent();
+                nString.putExtra("newItem", newText);
+                nString.putExtra("pos", pos);
+                setResult(RESULT_OK, nString);
+                finish();
+			}
+		});	editItem.setSelection(editItem.getText().length());
 	}
-
 }
--- SimpleToDo/src/codepath/apps/simpletodo/TodoActivity.java
+++ SimpleToDo/src/codepath/apps/simpletodo/TodoActivity.java
@@ -3,6 +3,7 @@ package codepath.apps.simpletodo;
 import codepath.apps.simpletodo.R;
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
@@ -18,10 +19,12 @@ import java.util.ArrayList;
 import org.apache.commons.io.FileUtils;
 
 public class TodoActivity extends Activity {
+	private static final int WRONG_POS = -1;
 	private ArrayList<String> items;
 	private ArrayAdapter<String> itemsAdapter;
 	private ListView lvItems;
 	private EditText etNewItem;
+	private int REQUEST_CODE;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -29,13 +32,26 @@ public class TodoActivity extends Activity {
 		setContentView(R.layout.activity_todo);
 		lvItems = (ListView) findViewById(R.id.lvItems);
 		etNewItem = (EditText) findViewById(R.id.etNewItem);
-		readItems();
+		readItems();	// populate the ArrayList (items), using file or blank if no file exists
 		itemsAdapter = new ArrayAdapter<String>(this,
 				android.R.layout.simple_list_item_1, items);
 		lvItems.setAdapter(itemsAdapter);
 		setupListViewListener();
 	}
 
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		//TODO check request code for error
+		if (resultCode == RESULT_OK) {
+			String newText = data.getExtras().getString("newItem");
+			int pos = data.getIntExtra("pos", WRONG_POS);
+			//TODO check for pos error
+			items.set(pos, newText);
+            itemsAdapter.notifyDataSetChanged();
+            saveItems();
+		}
+	}
+	
 	public void addTodoItem(View v) {
 		itemsAdapter.add(etNewItem.getText().toString());
 		etNewItem.setText("");
@@ -43,7 +59,7 @@ public class TodoActivity extends Activity {
 	}
 	
 	private void setupListViewListener() {
-		lvItems.setOnItemLongClickListener(new OnItemLongClickListener() {
+		lvItems.setOnItemLongClickListener(new OnItemLongClickListener() {	// Long click listener on item
 			@Override
 			public boolean onItemLongClick(AdapterView<?> aView, View item,
 					int pos, long id) {
@@ -53,15 +69,16 @@ public class TodoActivity extends Activity {
 				return true;
 			}
 		});
+// Click listener on item
 		lvItems.setOnItemClickListener(new OnItemClickListener() {
-
 			@Override
-			public void onItemClick(AdapterView<?> arg0, View arg1, int arg2,
-					long arg3) {
+			public void onItemClick(AdapterView<?> aView, View v, int pos,
+					long id) {
 				Intent eiIntent = new Intent(TodoActivity.this, EditItemActivity.class);
-				
+				eiIntent.putExtra("itemText", items.get(pos));
+				eiIntent.putExtra("pos", pos);
+				startActivityForResult(eiIntent, REQUEST_CODE);
 			}
-
 		});
 	}
 	
